856916617001db03febfbbc7582e26ac6d2a0690
Leoliyiang <31967637+Leoliyiang@users.noreply.github.com>
2017-12-13 08:53:51 -0500

    c

--- CHAPTER 7.py
+++ CHAPTER 7.py
@@ -1,5 +1,20 @@
-mylist=[0,2,3,7,11,12]
-mylist2=["apple", "banana","watermelon","grape","pear","orange"]
+import sys
+
+def test(did_pass):
+    """  Print the result of a test.  """
+    linenum = sys._getframe(1).f_lineno   # Get the caller's line number.
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+    
+def test_suite():
+    test(sum_even_nofirst(mylist)==-16)
+    test(sam(mylist2)==5)
+    
+mylist=[-13,-3,0,2,3,7,11,12]
+mylist2=["apple", "banana","watermelon","grape","sam","pear","orange"]
 def count_odd(numlist):
     count=0
     for n in numlist:
@@ -33,13 +48,23 @@ def sum_even_nofirst(numlist):
     sum=0
     for n in numlist:
         if n%2 == 0:
-            sum=0
             break
-    for n in numlist:
-        if n%2 ==0:
+        else:
             sum+=n
     return sum
 
+def sam(numlist):
+    count=0
+    for i in numlist:
+        if i == "sam":
+            count += 1
+            break
+        else:
+            count += 1
+    return count
+        
+    
+test_suite()
 print(count_odd(mylist))
 print(sum_even(mylist))
 print(sum_neg(mylist))
