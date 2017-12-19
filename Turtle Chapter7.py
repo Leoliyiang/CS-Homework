dd5a7f337bc532fc0e419e2e33b99ebeae610037
Leoliyiang <31967637+Leoliyiang@users.noreply.github.com>
2017-12-19 13:56:54 -0500

    improvement

--- Turtle Chapter7.py
+++ Turtle Chapter7.py
@@ -4,9 +4,14 @@ lo = turtle.Turtle()
 #for (angle,distance) in [(160, 20), (-43, 10), (270, 8), (-43, 12)]:
  #    lo.left(angle)
   #   lo.forward(distance)
+s=100
+r=(s**2/2)**0.5
+d=(2*s**2)**.5
  
-for (angle_house,distance_house) in [(60, 100), (-120, 100), (-120, 100), (90,100),(90,100),(90,100),(135,100*2**(1/2)),(135,100),(135,100*2**(1/2))]:
+path = [(90,s),(-45,r),(-90,r),(-45,s),(-135,d),(-135,s),(-135,d),(135,s)]
+for (angle_house,distance_house) in path:
      lo.left(angle_house)
      lo.forward(distance_house)
+
 print(lo.heading())  
 wn.mainloop()
