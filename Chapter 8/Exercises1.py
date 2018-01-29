20ecbf440d4b57e4060524b6170e2eaa52e5ea0c
Leoliyiang <31967637+Leoliyiang@users.noreply.github.com>
2018-01-29 17:04:46 -0500

    chapter8

---
+++ Chapter 8/Exercises1.py
@@ -0,0 +1,13 @@
+name="stef"
+newname=name.capitalize()
+print(newname)
+
+prefixes = "JKLMNOPQ"
+suffix = "ack"
+
+for p in prefixes:
+    while p==prefixes[5] or p==prefixes[7]:
+        print(p + "u" + suffix)
+
+
+
